25a4787a0b6a5eb9e36c3a5869c0056cf19b8085
Lee Damien <notesong.git@outlook.com>
2020-01-07 22:28:08 -0500

    Add code to perform operations

--- app/src/main/java/learningprogramming/academy/calculator/MainActivity.kt
+++ app/src/main/java/learningprogramming/academy/calculator/MainActivity.kt
@@ -62,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         button9.setOnClickListener(listener)
         buttonDecimal.setOnClickListener(listener)
 
-        val opListener = View.OnClickListener { v->
+        val opListener = View.OnClickListener { v ->
             val op = (v as Button).text.toString()
             val value = newNumber.text.toString()
             if(value.isNotEmpty()) {
@@ -77,5 +77,29 @@ class MainActivity : AppCompatActivity() {
         buttonMinus.setOnClickListener(opListener)
         buttonMultiply.setOnClickListener(opListener)
         buttonDivide.setOnClickListener(opListener)
+    }
+
+    private fun performOperation(value: String, operation: String) {
+        if (operand1 == null) {
+            operand1 = value.toDouble()
+        } else {
+            operand2 = value.toDouble()
+            if (pendingOperation == "=") {
+                pendingOperation = operation
+            }
+            when (pendingOperation) {
+                "=" -> operand1 = operand2
+                "/" -> if (operand2 == 0.0) {
+                            operand1 = Double.NaN // handle attempt to divide by zero
+                        } else {
+                            operand1 = operand1!! / operand2
+                        }
+                "*" -> operand1 = operand1!! * operand2
+                "-" -> operand1 = operand1!! - operand2
+                "+" -> operand1 = operand1!! + operand2
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
     }
 }
